666ca3e0592bc1e6ca55eb59bee78f9c94a398f7
Kevin Lin <kevinlin.vxd@gmail.com>
2022-02-18 02:48:57 -0600

    Finished test. Working out bugs

--- typingspeed.py
+++ typingspeed.py
@@ -1,5 +1,3 @@
-from email import message
-from this import d
 import pygame
 from pygame.locals import *
 import sys
@@ -27,7 +25,7 @@ class Game:
 
         pygame.init()
         self.open_img = pygame.image.load('startup.jpg')
-        self.open_img = pygame.transform.scale(self.open_img, (self.weight, self.height))
+        self.open_img = pygame.transform.scale(self.open_img, (self.width, self.height))
         self.bg = pygame.image.load('background.jpg')
         self.bg = pygame.transform.scale(self.bg, (500, 750))
         self.screen = pygame.display.set_mode((self.width, self.height))
@@ -35,18 +33,19 @@ class Game:
 
     def draw_text(self, screen, message, y, font_size, color):
         font = pygame.font.Font(None, font_size)
-        text = font.render(message, color)
-        text_rect = text.get_rect(center=(self.width/2, y))
+        text = font.render(message, True, color)
+        text_rect = text.get_rect(center=(self.width / 2, y))
         screen.blit(text, text_rect)
         pygame.display.update()
 
-    def get_sentence(self):
+    @staticmethod
+    def get_sentence():
         file = open('sentences.txt').read()
         sentences = file.split('\n')
         sentence = random.choice(sentences)
         return sentence
 
-    def reset(self):
+    def reset_test(self):
         self.screen.blit(self.open_img, (0, 0))
         pygame.display.update()
         time.sleep(1)
@@ -55,27 +54,27 @@ class Game:
         self.active = False
         self.end = False
         self.input_text = ''
-        self.word = ''
+        self.random_sentence = ''
         self.start_time = 0
         self.total_time = 0
         self.wpm = 0
         self.random_sentence = self.get_sentence
-        if (not self.word):
-            self.reset
+        if not self.random_sentence:
+            self.reset_test()
 
-        self.screen.fill(0, 0, 0)
+        self.screen.fill((0, 0, 0))
         self.screen.blit(self.bg, (0, 0))
         message = 'Typing Speed Test'
         self.draw_text(self.screen, message, 80, 80, self.HEAD_C)
         pygame.draw.rect(self.screen, (255, 192, 25), (50, 250, 650, 50), 2)
 
-        self.draw_text(self.screen, self.random_sentence, 28, self.TEXT_C)
+        self.draw_text(self.screen, self.random_sentence, 200, 28, self.TEXT_C)
         pygame.display.update()
 
     def run(self):  # TODO
-        self.reset()
+        self.reset_test()
         self.running = True
-        while (self.running):
+        while self.running:
             clock = pygame.time.Clock()
             self.screen.fill((0, 0, 0), (50, 250, 650, 50))
             pygame.draw.rect(self.screen, self.HEAD_C, (50, 250, 650, 50), 2)
@@ -84,23 +83,25 @@ class Game:
             for event in pygame.event.get():
                 if event.type == QUIT:
                     self.running = False
-                    sys.exit()
+                    pygame.display.quit()
+                    pygame.quit()
+                    quit()
                 elif event.type == pygame.MOUSEBUTTONUP:
                     x, y = pygame.mouse.get_pos()
-                    if (x >= 50 and x <= 650 and y >= 250 and y < 350):
+                    if 50 <= x <= 650 and 250 <= y < 350:
                         self.active = True
                         self.input_text = ''
                         self.start_time = time.time()
-                    if (x >= 310 and x <= 510 and y >= 390 and self.end):
-                        self.reset_game()
+                    if 310 <= x <= 510 and y >= 390 and self.end:
+                        self.reset_test()
                         x, y = pygame.mouse.get_pos()
                 elif event.type == pygame.KEYDOWN:
                     if self.active and not self.end:
                         if event.key == pygame.K_RETURN:
                             print(self.input_text)
                             self.show_stats(self.screen)
-                            print(self.stats)
-                            self.draw_text(self.screen, self.stats, 350, 28, self.RESULT_C)
+                            print(self.results)
+                            self.draw_text(self.screen, self.results, 350, 28, self.RESULT_C)
                             self.end = True
                         elif event.key == pygame.K_BACKSPACE:
                             self.input_text = self.input_text[:-1]
@@ -112,5 +113,32 @@ class Game:
             pygame.display.update()
         clock.tick(60)
 
-    def show_stats(self):  # TODO
-        print('TODO')
+    def show_stats(self, screen):
+        if not self.end:
+            self.total_time = time.time() - self.start_time
+
+            count = 0
+            for i, c in enumerate(self.random_sentence):
+                try:
+                    if self.input_text[i] == c:
+                        count += 1
+                except:
+                    pass
+            self.accuracy = count / len(self.random_sentence) * 100
+            self.wpm = len(self.input_text) * 60 / (5 * self.total_time)
+            self.end = True
+            print(self.total_time)
+
+            self.results = 'Time:' + str(round(self.total_time)) + " secs   Accuracy:" + str(
+                round(self.accuracy)) + "%" + '   Wpm: ' + str(round(self.wpm))
+
+            self.time_img = pygame.image.load('icon.png')
+            self.time_img = pygame.transform.scale(self.time_img, (150, 150))
+            screen.blit(self.time_img, (self.w / 2 - 75, self.h - 140))
+            self.draw_text(screen, "Reset", self.h - 70, 26, (100, 100, 100))
+
+            print(self.results)
+            pygame.display.update()
+
+
+Game().run()
